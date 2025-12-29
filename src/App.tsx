68ca6605046ad9e2c9ecd714fea4dfdc9ddd9f90
enwkurs <erinwajca@gmail.com>
2025-12-29 13:38:11 +0100

    fff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import { Pokemon } from './components/apitest'
+// import { Pokemon } from './components/apitest'
 import { PokemonTypes } from './components/typeslist'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
